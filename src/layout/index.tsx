70087c1f4996182cce1a67d9f54dc0d7e5a48f70
imMatheus <matheusmendesbarata03@gmail.com>
2022-10-29 16:30:52 +0200

    formatted

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Dock from '../abcdef/src/components/dock'
+import React from "react";
+import Dock from "../abcdef/src/components/dock";
 
 interface MainProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const Main: React.FC<MainProps> = ({ children }) => {
-    return (
-        <div className='flex h-screen flex-col'>
-            <div className='relative flex-1 bg-teal-400'>{children}</div>
-            <Dock />
-        </div>
-    )
-}
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="relative flex-1 bg-teal-400">{children}</div>
+      <Dock />
+    </div>
+  );
+};
 
-export default Main
+export default Main;
